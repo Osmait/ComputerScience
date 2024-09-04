604b12553b820d8f7d771e8b49ce8d56f564e584
Saul Burgos <saulburgos6@gmail.com>
2024-09-03 20:08:34 -0400

    add linked list on python

--- DataStructures/LinkedList/Python/main.py
+++ DataStructures/LinkedList/Python/main.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from collections.abc import Iterable
 from dataclasses import dataclass
 from typing import Any
@@ -14,7 +15,7 @@ class Node:
 
 class LinkedList:
     def __init__(self) -> None:
-        self.head = None
+        self.head: Node | None = None
 
     def __iter__(self) -> Iterable[Any]:
         node = self.head
@@ -23,7 +24,7 @@ class LinkedList:
             node = node.next_node
 
     def __len__(self) -> int:
-        return sum(i for _ in self)
+        return sum(1 for _ in self)
 
     def __repr__(self) -> str:
         return "->".join([str(item) for item in self])
@@ -35,3 +36,108 @@ class LinkedList:
             if i == index:
                 return node
         return None
+
+    def __setitem__(self, index: int, data: any):
+        if not 0 <= index < len(self):
+            raise IndexError("Index out of range")
+        current: Node = self.head
+        for _ in range(index):
+            current = current.next_node
+        current.data = data
+
+    def insert_nth(self, index: int, data: Any) -> None:
+        if not 0 <= index <= len(self):
+            raise IndexError("list index out of range")
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        elif index == 0:
+            new_node.next_node = self.head
+            self.head = new_node
+        else:
+            temp = self.head
+            for _ in range(index - 1):
+                temp = temp.next_node
+            new_node.next_node = temp.next_node
+            temp.next_node = new_node
+
+    def insert_tail(self, data: Any) -> None:
+        self.insert_nth(len(self), data)
+
+    def insert_head(self, data: Any) -> None:
+        self.insert_nth(0, data)
+
+    def print_list(self) -> None:
+        print(self)
+
+    def delete_nth(self, index: int = 0) -> Any:
+        if not 0 <= index <= len(self) - 1:
+            raise IndexError("List Index out of range")
+        delete_node = self.head
+        if index == 0:
+            self.head = self.head.next_node
+        else:
+            temp = self.head
+            for _ in range(index - 1):
+                temp = temp.next_node
+            delete_node = temp.next_node
+            temp.next_node = temp.next_node.next_node
+        return delete_node.data
+
+    def delete_head(self) -> Any:
+        return self.delete_nth(0)
+
+    def delete_tail(self) -> Any:
+        return self.delete_nth(len(self) - 1)
+
+    def is_empty(self) -> bool:
+        return self.head is None
+
+    def reverse(self) -> None:
+        prev = None
+        current = self.head
+
+        while current:
+            next_node = current.next_node
+            current.next_node = prev
+            prev = current
+            current = next_node
+        self.head = prev
+
+
+def main():
+    from doctest import testmod
+
+    testmod()
+
+    linked_list = LinkedList()
+    linked_list.insert_head(input("Inserting 1st at head ").strip())
+    linked_list.insert_head(input("Inserting 2nd at head ").strip())
+    print("\nPrint list:")
+    linked_list.print_list()
+    linked_list.insert_tail(input("\nInserting 1st at tail ").strip())
+    linked_list.insert_tail(input("Inserting 2nd at tail ").strip())
+    print("\nPrint list:")
+    linked_list.print_list()
+    print("\nDelete head")
+    linked_list.delete_head()
+    print("Delete tail")
+    linked_list.delete_tail()
+    print("\nPrint list:")
+    linked_list.print_list()
+    print("\nReverse linked list")
+    linked_list.reverse()
+    print("\nPrint list:")
+    linked_list.print_list()
+    print("\nString representation of linked list:")
+    print(linked_list)
+    print("\nReading/changing Node data using indexing:")
+    print(f"Element at Position 1: {linked_list[1]}")
+    linked_list[1] = input("Enter New Value: ").strip()
+    print("New list:")
+    print(linked_list)
+    print(f"length of linked_list is : {len(linked_list)}")
+
+
+if __name__ == "__main__":
+    main()

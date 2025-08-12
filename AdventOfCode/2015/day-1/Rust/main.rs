e0e9b90b368d944a296000e07d5cfec84f535614
osmait <saulburgos6@gmail.com>
2025-08-11 22:46:50 -0400

    add day-1 rust

--- AdventOfCode/2015/day-1/Rust/main.rs
+++ AdventOfCode/2015/day-1/Rust/main.rs
@@ -1,12 +1,32 @@
-use std::{
-    fs::{read_to_string, File},
-    io::Read,
-};
+use std::{collections::HashMap, fs::read_to_string};
 
+fn part_1() {
+    let file = read_to_string("../input2.txt").expect("Failed to read file");
+    let clean_data = file.trim();
+    let hash = HashMap::from([('(', 1), (')', -1)]);
+    let mut total = 0;
+    clean_data.chars().for_each(|p| {
+        let value = hash.get(&p).expect("erroroorr  ");
+        total += value
+    });
+    println!("{}", total)
+}
+fn part_2() {
+    let file = read_to_string("../input2.txt").expect("Failed to read file");
+    let clean_data = file.trim();
+    let hash = HashMap::from([('(', 1), (')', -1)]);
+    let mut total = 0;
+    let mut basement = 0;
+    clean_data.chars().for_each(|p| {
+        let value = hash.get(&p).expect("error");
+        basement += 1;
+        total += value;
+        if total == -1 {
+            println!("test {}", basement);
+        }
+    });
+    println!("{}", total)
+}
 fn main() {
-    // let file = read_to_string("../input.txt").unwrap();
-    let mut input = File::open("../input.txt").unwrap();
-    let mut file = String::new();
-    input.read_to_string(&mut file).unwrap();
-    println!("{}", file)
+    part_2();
 }
